c8b7afba28f0671b651b75033903c35063ce524b
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-08-12 09:50:21 +0530

    feat: segmentation changes

--- components/Responses/RoadmapBox.tsx
+++ components/Responses/RoadmapBox.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 interface RoadmapBoxProps {
   id: string;
@@ -6,17 +6,32 @@ interface RoadmapBoxProps {
   valueText: string;
   isLast: boolean;
   isThird: boolean;
-  onClick: () => void
+  onClick: () => void;
 }
 
-const RoadmapBox: React.FC<RoadmapBoxProps> = ({ id, keyText, valueText, isLast, isThird, onClick }) => {
+const RoadmapBox: React.FC<RoadmapBoxProps> = ({
+  id,
+  keyText,
+  valueText,
+  isLast,
+  isThird,
+  onClick,
+}) => {
   return (
-    <div id={id} onClick={onClick} className="relative flex flex-col items-center p-10 m-2 rounded-3xl border-[2px] border-zinc-600 text-center bg-gradient-to-r from-zinc-800 to-black-100 text-white shadow-2xl hover:border-zinc-700 cursor-pointer">
+    <div
+      id={id}
+      onClick={onClick}
+      className="relative flex flex-col items-center p-10 m-2 rounded-3xl border-[2px] border-zinc-600 text-center bg-gradient-to-r from-zinc-800 to-black-100 text-white shadow-2xl hover:border-zinc-700 cursor-pointer"
+    >
       <p className="font-medium text-lg text-gray-400">{keyText}</p>
       <p>{valueText}</p>
       {!isLast && (
         <div
-          className={`absolute ${isThird ? 'bottom-0 left-1/2 transform -translate-x-1/2 translate-y-full' : 'top-1/2 -rotate-90 right-0 transform translate-x-full -translate-y-1/2'} h-8 w-8`}
+          className={`absolute ${
+            isThird
+              ? "bottom-0 left-1/2 transform -translate-x-1/2 translate-y-full"
+              : "top-1/2 -rotate-90 right-0 transform translate-x-full -translate-y-1/2"
+          } h-8 w-8`}
         >
           <svg
             className="w-full h-full"
